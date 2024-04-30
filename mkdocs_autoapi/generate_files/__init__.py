d4fcc474ab324af371f56b2f952ca484a7cbc1b4
jcayers20 <jcayers20@gmail.com>
2024-04-29 20:56:30 -0500

    First pass at incorporating mkdocs_gen_files functionality
    This is largely a copy-paste of logic found in `mkdocs-gen-files` with some
    light refactoring applied. In the near future I'll take a deep look at all this
    logic, adding documentation and seeking opportunities to make it more readable
    and/or performant. For now, though, the logic works as expected.

---
+++ mkdocs_autoapi/generate_files/__init__.py
@@ -0,0 +1,10 @@
+"""Init file for the generate_files module.
+
+As I work through the build, I'll update the documentation for this module.
+"""
+
+from .editor import FilesEditor
+
+
+def __getattr__(name: str):
+    return getattr(FilesEditor.current(), name)
