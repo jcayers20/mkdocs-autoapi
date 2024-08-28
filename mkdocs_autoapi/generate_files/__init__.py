6995ff5835f647e9ec89ab40c5c26aa28c445e68
jcayers20 <jcayers20@gmail.com>
2024-08-28 09:04:34 -0500

    Use explicit imports rather than star imports
    `ruff check` surfaced a few errors related to using `*` imports, so I modified
    these imports to be explicit.

--- mkdocs_autoapi/generate_files/__init__.py
+++ mkdocs_autoapi/generate_files/__init__.py
@@ -3,8 +3,8 @@
 As I work through the build, I'll update the documentation for this module.
 """
 
-from .nav import *
-from .editor import *
+from .nav import Nav as Nav
+from .editor import FilesEditor
 
 
 def __getattr__(name: str):
